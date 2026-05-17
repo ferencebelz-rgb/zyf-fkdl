67ae241137f6fc536f9d48d5d80f90d5fe65a7d2
hanshanyu <m17641521235@163.com>
2026-05-17 10:19:49 +0800

    task1 直角弯目标改为 8 个

--- code/task1.h
+++ code/task1.h
@@ -3,7 +3,7 @@
 
 #include "zf_common_headfile.h"
 
-#define TASK1_TURN_TARGET  1     /* 目标直角弯个数 */
+#define TASK1_TURN_TARGET  8     /* 目标直角弯个数 */
 
 void Task1_Init(void);
 void Task1_Update(void);
